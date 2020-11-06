b2d7972095717de048b4da2e003639135333ae6b
Halozy-bit <jamalludin1999z@gmail.com>
2020-11-06 12:03:52 +0700

    menambahkan library dan menginisialisasi metode klasifikasi

--- klasifikasi_asli.py
+++ klasifikasi_asli.py
@@ -1,4 +1,5 @@
 #FILE MENCOBA DATA DIMENSI BANYAK DENGAN METODE KLASIFIKASI TANPA TAMBAHAN PREPROCESING
+
 #Library
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -11,3 +12,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_iris
+
+#Membuat inisialisasi tiap metode 
+models = {
+    "knn": KNeighborsClassifier(n_neighbors=1),
+    "naive_bayes": GaussianNB(),
+    "logit": LogisticRegression(solver="lbfgs", multi_class="auto"),
+    "svm": SVC(kernel="rbf", gamma="auto"),
+    "decision_tree": DecisionTreeClassifier(),
+    "random_forest": RandomForestClassifier(n_estimators=100),
+    "mlp": MLPClassifier()
+}
